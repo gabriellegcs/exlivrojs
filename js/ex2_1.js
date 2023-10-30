e430406567232916b623922206ed55abf097d3be
Gabrielle <135549255+gabriellegcs@users.noreply.github.com>
2023-10-30 20:33:03 -0300

    Add files via upload

---
+++ js/ex2_1.js
@@ -0,0 +1,14 @@
+
+// cria referência ao form e ao elemento h3 (onde será exibida a resposta):
+
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3");
+
+// cria um ouvinte de evento, acionando quando o botão submit for clicado:
+
+frm.addEventListener ("submit", (e) => {
+    const nome = frm.inNome.value // obtém o nome digitado no form
+    resp.innerText = `Olá, ${nome}` // exibe a respsota do programa
+    e.preventDefault()  //evita envio do form
+})
+
